1a6f69b31a0b7620dcb46e96a4f6c0b4db4faca5
Gustavo Araujo Pires <gustavoaraujopires9@gmail.com>
2026-04-13 11:24:59 -0300

    Projeto inicial e-commerce

---
+++ src/test/java/com/github/GustavoAraujoPires/Projeto/e_commerce/ProjetoECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.GustavoAraujoPires.Projeto.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
