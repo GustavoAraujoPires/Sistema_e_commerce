a8e395fd06faea3092794301f188b0c7893d997f
Gustavo Araujo Pires <gustavoaraujopires9@gmail.com>
2026-04-21 18:30:25 -0300

    Validando o email se já existe

--- src/main/java/com/github/GustavoAraujoPires/Projeto/e_commerce/controller/common/GlobalExceptionHandler.java
+++ src/main/java/com/github/GustavoAraujoPires/Projeto/e_commerce/controller/common/GlobalExceptionHandler.java
@@ -2,12 +2,14 @@ package com.github.GustavoAraujoPires.Projeto.e_commerce.controller.common;
 
 import com.github.GustavoAraujoPires.Projeto.e_commerce.controller.dto.ErroCampo;
 import com.github.GustavoAraujoPires.Projeto.e_commerce.controller.dto.ErroResposta;
+import com.github.GustavoAraujoPires.Projeto.e_commerce.exception.ClienteInvalidoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,5 +27,15 @@ public class GlobalExceptionHandler {
         return new ErroResposta(
                 HttpStatus.UNPROCESSABLE_ENTITY.value(),
                 "Erro de validação", listaErro);
+    }
+
+    @ExceptionHandler(ClienteInvalidoException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handerClienteInvalidoException(ClienteInvalidoException e){
+        List<ErroCampo> listaErro = List.of(new ErroCampo("email", e.getMessage()));
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação",listaErro);
+
     }
 }
